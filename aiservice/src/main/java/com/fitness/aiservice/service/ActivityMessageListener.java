699cf225ff3bcd9cff557b29f2f5dd6077fea91b
MansiNA <mansikavathiya1998@gmail.com>
2025-06-26 09:49:16 +0200

    configure Gemini api and received activity

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,27 +1,35 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ActivityMessageListener {
-
-    @Value("${rabbitmq.queue.name}")
-    private String queue;
-    @Value("${rabbitmq.exchange.name}")
-    private String exchange;
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
+    private final ActivityAIService aiService;
+    private final RecommendationRepository recommendationRepository;
+    private final MongoTemplate mongoTemplate;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
+     //   log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        Recommendation recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
+        log.info("Saved Recommendation with ID: {}", recommendation.getId());
+        String dbName = mongoTemplate.getDb().getName();
+        String collectionName = mongoTemplate.getCollectionName(Recommendation.class);
+
+        log.info("MongoDB Database: {}", dbName);
+        log.info("MongoDB Collection: {}", collectionName);
     }
 
 }
